d9f208cdfe07e6714435daa624f89e2fff05be68
vl7sha <vl7shab@yandex.ru>
2024-03-06 23:55:28 +0400

    add auth

---
+++ src/main/java/ru/pishemzapuskayem/backendbookservice/controllers/AuthController.java
@@ -0,0 +1,70 @@
+package ru.pishemzapuskayem.backendbookservice.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+import ru.pishemzapuskayem.backendbookservice.exception.ApiException;
+import ru.pishemzapuskayem.backendbookservice.mapper.AccountMapper;
+import ru.pishemzapuskayem.backendbookservice.model.dto.AccountDTO;
+import ru.pishemzapuskayem.backendbookservice.model.dto.AuthRequestDTO;
+import ru.pishemzapuskayem.backendbookservice.model.dto.AuthResponseDTO;
+import ru.pishemzapuskayem.backendbookservice.model.entity.Account;
+import ru.pishemzapuskayem.backendbookservice.security.UserDetailsImpl;
+import ru.pishemzapuskayem.backendbookservice.security.jwt.JwtUtil;
+import ru.pishemzapuskayem.backendbookservice.service.AuthService;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AccountMapper accountMapper;
+    private final AuthenticationManager authenticationManager;
+    private final AuthService authService;
+    private final JwtUtil jwtUtil;
+
+    @Value("${jwt.tokenExpiresIn}")
+    private int tokenExpiresIn;
+
+    @GetMapping("/me")
+    public ResponseEntity<AccountDTO> getMe(){
+        return ResponseEntity.ok(
+                accountMapper.map(
+                        authService.getAuthenticated().get()
+                )
+        );
+    }
+
+    @PostMapping("/sign-in")
+    public ResponseEntity<AuthResponseDTO> login(@RequestBody AuthRequestDTO authRequestDTO){
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                authRequestDTO.getEmail(),
+                authRequestDTO.getPassword()
+        );
+
+        Authentication authentication;
+
+        try {
+            authentication = authenticationManager.authenticate(authenticationToken);
+        } catch (BadCredentialsException e) {
+            throw new ApiException("Неправильные логин или пароль");
+        }
+
+        Account account = ((UserDetailsImpl)authentication.getPrincipal()).getUser();
+        String token = jwtUtil.generateToken(
+                account.getEmail(),
+                account.getRole().getName(),
+                tokenExpiresIn
+        );
+
+        return ResponseEntity.ok(new AuthResponseDTO(
+                token,
+                tokenExpiresIn
+        ));
+    }
+}
---
+++ src/main/java/ru/pishemzapuskayem/backendbookservice/service/AuthService.java
@@ -0,0 +1,24 @@
+package ru.pishemzapuskayem.backendbookservice.service;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.pishemzapuskayem.backendbookservice.model.entity.Account;
+import ru.pishemzapuskayem.backendbookservice.security.UserDetailsImpl;
+
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+public class AuthService {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public Optional<Account> getAuthenticated() {
+        UserDetailsImpl userDetails = (UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Account detachedAccount = userDetails.getUser();
+        return Optional.ofNullable(entityManager.find(Account.class, detachedAccount.getId()));
+    }
+}
